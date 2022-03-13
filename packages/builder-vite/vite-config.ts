101d2d149591736d3a68e6b1d5a5c98c64d2b558
Kirill Romanov <djaler1@gmail.com>
2022-03-13 22:28:30 +0300

    feat: adapt vite-config for vue 2

--- packages/builder-vite/vite-config.ts
+++ packages/builder-vite/vite-config.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 import fs from 'fs';
 import { Plugin } from 'vite';
-import { TypescriptConfig } from '@storybook/core-common';
 import viteReact from '@vitejs/plugin-react';
 
 import { allowedEnvPrefix as envPrefix } from './envs';
@@ -32,30 +31,22 @@ export async function commonConfig(
   options: ExtendedOptions,
   _type: PluginConfigType
 ): Promise<UserConfig & { configFile: false; root: string }> {
-  const { framework } = options;
-
   return {
     configFile: false,
     root: path.resolve(options.configDir, '..'),
     cacheDir: 'node_modules/.vite-storybook',
     envPrefix,
     define: {},
-    resolve:
-      framework === 'vue3'
-        ? {
-            alias: {
-              vue: 'vue/dist/vue.esm-bundler.js',
-            },
-          }
-        : {},
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.esm.js',
+      },
+    },
     plugins: await pluginConfig(options, _type),
   };
 }
 
 export async function pluginConfig(options: ExtendedOptions, _type: PluginConfigType) {
-  const { framework, presets } = options;
-  const svelteOptions: Record<string, any> = await presets.apply('svelteOptions', {}, options);
-
   const plugins = [
     codeGeneratorPlugin(options),
     mockCoreJs(),
@@ -82,114 +73,21 @@ export async function pluginConfig(options: ExtendedOptions, _type: PluginConfig
       },
     },
   ] as Plugin[];
-  if (framework === 'vue' || framework === 'vue3') {
+
     try {
-      const vuePlugin = require('@vitejs/plugin-vue');
-      plugins.push(vuePlugin());
+      const { createVuePlugin } = require('vite-plugin-vue2');
+      plugins.push(createVuePlugin());
       const { vueDocgen } = await import('./plugins/vue-docgen');
       plugins.push(vueDocgen());
     } catch (err) {
       if ((err as NodeJS.ErrnoException).code === 'MODULE_NOT_FOUND') {
         throw new Error(
-          '@storybook/builder-vite requires @vitejs/plugin-vue to be installed ' +
-            'when using @storybook/vue or @storybook/vue3.' +
+          'storybook-builder-vite-vue2 requires vite-plugin-vue2 to be installed.' +
             '  Please install it and start storybook again.'
         );
       }
       throw err;
     }
-  }
-  if (framework === 'svelte') {
-    try {
-      const sveltePlugin = require('@sveltejs/vite-plugin-svelte').svelte;
-
-      // We need to create two separate svelte plugins, one for stories, and one for other svelte files
-      // because stories.svelte files cannot be hot-module-reloaded.
-      // Suggested in: https://github.com/sveltejs/vite-plugin-svelte/issues/321#issuecomment-1113205509
-
-      // First, create an array containing user exclude patterns, to combine with ours.
-      const userExclude = Array.isArray(svelteOptions?.exclude)
-        ? svelteOptions?.exclude
-        : svelteOptions?.exclude
-        ? [svelteOptions?.exclude]
-        : [];
-
-      // These are the svelte stories we need to exclude from HMR
-      const storyPatterns = ['**/*.story.svelte', '**/*.stories.svelte'];
-      // Non-story svelte files
-      // Starting in 1.0.0-next.42, svelte.config.js is included by default.
-      // We disable that, but allow it to be overridden in svelteOptions
-      plugins.push(sveltePlugin({ ...svelteOptions, exclude: [...userExclude, ...storyPatterns] }));
-      // Svelte stories without HMR
-      const storySveltePlugin = sveltePlugin({
-        ...svelteOptions,
-        exclude: userExclude,
-        include: storyPatterns,
-        hot: false,
-      });
-      plugins.push({
-        // Starting in 1.0.0-next.43, the plugin function returns an array of plugins.  We only want the first one here.
-        ...(Array.isArray(storySveltePlugin) ? storySveltePlugin[0] : storySveltePlugin),
-        name: 'vite-plugin-svelte-stories',
-      });
-    } catch (err) {
-      if ((err as NodeJS.ErrnoException).code === 'MODULE_NOT_FOUND') {
-        throw new Error(
-          '@storybook/builder-vite requires @sveltejs/vite-plugin-svelte to be installed' +
-            ' when using @storybook/svelte.' +
-            '  Please install it and start storybook again.'
-        );
-      }
-      throw err;
-    }
-
-    const { loadSvelteConfig } = require('@sveltejs/vite-plugin-svelte');
-    const config = { ...loadSvelteConfig(), ...svelteOptions };
-
-    try {
-      const csfPlugin = require('./svelte/csf-plugin').default;
-      plugins.push(csfPlugin(config));
-    } catch (err) {
-      // Not all projects use `.stories.svelte` for stories, and by default 6.5+ does not auto-install @storybook/addon-svelte-csf.
-      // If it's any other kind of error, re-throw.
-      if ((err as NodeJS.ErrnoException).code !== 'MODULE_NOT_FOUND') {
-        throw err;
-      }
-    }
-
-    const { svelteDocgen } = await import('./plugins/svelte-docgen');
-    plugins.push(svelteDocgen(config));
-  }
-
-  if (framework === 'preact') {
-    plugins.push(require('@preact/preset-vite').default());
-  }
-
-  if (framework === 'react') {
-    const { reactDocgen, reactDocgenTypescriptOptions } = await presets.apply('typescript', {} as TypescriptConfig);
-
-    let typescriptPresent;
-
-    try {
-      const pkgJson = readPackageJson();
-      typescriptPresent = pkgJson && (pkgJson?.devDependencies?.typescript || pkgJson?.dependencies?.typescript);
-    } catch (e) {
-      typescriptPresent = false;
-    }
-
-    if (reactDocgen === 'react-docgen-typescript' && typescriptPresent) {
-      plugins.push(require('@joshwooding/vite-plugin-react-docgen-typescript').default(reactDocgenTypescriptOptions));
-    } else if (reactDocgen) {
-      const { reactDocgen } = await import('./plugins/react-docgen');
-      // Needs to run before the react plugin, so add to the front
-      plugins.unshift(reactDocgen());
-    }
-  }
-
-  if (framework === 'glimmerx') {
-    const plugin = require('vite-plugin-glimmerx/index.cjs');
-    plugins.push(plugin.default());
-  }
 
   return plugins;
 }
